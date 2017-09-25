427ea1a56e1b86490edf55531e4ad10a2701589b
Michael Sweeney <sweeney.pro@hotmail.co.uk>
2017-09-25 20:29:10 +0100

    Initial Commit

---
+++ TestProject/Assets/MovementAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAI : MonoBehaviour {
+
+    // Kinematic
+    public Vector3 position;
+    public float orientation;
+    public Vector3 velocity;
+    public float rotation;
+
+    // SteeringOutput
+    public Vector3 linear;
+    public float angular;
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        position += velocity * Time.deltaTime;
+        orientation += rotation * Time.deltaTime;
+
+        velocity += linear * Time.time;
+        orientation += angular * Time.time;
+	}
+
+    
+}
